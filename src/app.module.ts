299fafde3921184f74eafc8261676b3d7905ef72
soheib <soheib.mohammadi95@gmail.com>
2024-05-26 04:39:30 +0330

    add queue system to f2m crawler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DataModule } from './data/data.module';
-import { F2mModule } from './app/f2m/f2m.module';
-import { MovieModule } from './app/movie/movie.module';
-import { TelegramBotModule } from './app/bot/bot.module';
-import { OpenAiModule } from './app/openai/openai.module';
+import { F2mModule } from './application/f2m/f2m.module';
+import { MovieModule } from './application/movie/movie.module';
+import { TelegramBotModule } from './application/bot/bot.module';
+import { OpenAiModule } from './application/openai/openai.module';
 import { BullModule } from '@nestjs/bull';
 
 @Module({
