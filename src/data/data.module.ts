e2b3142baf2d17f71cc5b1d94cf25bcfe8c861ac
soheib <soheib.mohammadi95@gmail.com>
2024-08-05 02:20:33 +0330

    add 27017 docker

--- src/data/data.module.ts
+++ src/data/data.module.ts
@@ -24,7 +24,7 @@ import { ISiteRepository } from 'src/core/interfaces/crawler/ISite-repository';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGODB_URL, {}),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
     MongooseModule.forFeature([
       { name: Site.name, schema: SiteSchema },
       { name: PaginationUrl.name, schema: PaginationUrlSchema },
