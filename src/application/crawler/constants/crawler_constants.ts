17ce8bd764dbf2a0088c790ecf62ab068c58417d
soheib <soheib.mohammadi95@gmail.com>
2024-08-08 02:29:15 +0330

    update docker compose file to add redis

--- src/application/crawler/constants/crawler_constants.ts
+++ src/application/crawler/constants/crawler_constants.ts
@@ -1,4 +1,4 @@
-export const F2MBaseUrl = 'https://www.f2m.fun/category/film/';
+export const F2MBaseUrl = 'https://www.film2mediax.ir/category/film/';
 export const F2MSiteName = 'f2m';
 export const ZarFilmBaseUrl = 'https://zarfilm.com/all-movie/';
 export const ZarFilmSiteName = 'zarfilm';
